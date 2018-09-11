5c06f120db26fd6a464ad367743ec6494a603095
Bruce McTigue <bmctigue@tiguer.com>
2018-09-11 13:29:31 -0600

    Removed access private so tests can run

--- CleanSwift/Scenes/CleanSwift/Interactor.swift
+++ CleanSwift/Scenes/CleanSwift/Interactor.swift
@@ -14,8 +14,8 @@ protocol BusinessLogic: class {
 
 class Interactor: BusinessLogic {
     
-    private var presenter: PresentationLogic!
-    private var worker: Worker!
+    var presenter: PresentationLogic!
+    var worker: Worker!
     
     init(presenter: PresentationLogic, store: StoreProtocol) {
         self.presenter = presenter
